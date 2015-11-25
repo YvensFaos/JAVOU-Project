30d021f1724ceef04657d16cec7902ab46138e20
duducorvao <eduardobruno@hotmail.co.uk>
2015-11-25 11:45:37 -0200

    Criado um objeto chamado ObjectDestroyer que tem um script chamado DestroyerScript que destroi qualquer objeto que toque nele. (feito pra deletar os inimigos spawnados e as balas deles)

---
+++ Assets/Scripts/DestroyerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+
+    //public void OnColliderEnter(Collision other)
+    //{
+    //    Destroy(other.gameObject);
+    //}
+}
--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -6,31 +6,52 @@ public class EnemyScript : MonoBehaviour {
     public float hp;
     public float speed;
     public float speedFactor; //Altera a velocidade do inimigo para gerar uma velocidade diferente para cada um instanciado.
+    public float shootCooldown;
+    private float shootTimer;
+
     public Transform shooterPosition;
 
     public Transform enemyBullet;
 
     private GameObject playerReference;
+    private Vector3 playerPosition;
 
 	// Use this for initialization
 	void Start () {
-
+        shootTimer = shootCooldown;
         speed *= Random.Range(1.0f, speedFactor);
         playerReference = GameObject.FindGameObjectWithTag("Player"); //Acha o objeto player através da tag Player.
+        playerPosition = playerReference.transform.position;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.Translate(-transform.up * speed, Space.World);
-        EnemyBehaviour();
+        shootTimer -= Time.deltaTime;
 
+        if (shootTimer <= 0)
+        {
+            Instantiate(enemyBullet, shooterPosition.position, transform.rotation);
+
+            shootTimer = shootCooldown;
+        }
+
+        transform.Translate(transform.up * speed, Space.World);
+        EnemyBehaviour();
 
 	}
 
     private void EnemyBehaviour()
     {
         //Lerp apenas de X
+        playerPosition = playerReference.transform.position;
+
+        Vector3 newPosition = transform.position;
+
+        newPosition.x = Mathf.Lerp(newPosition.x, playerPosition.x, speed / 8);
+
+        transform.position = newPosition;
+        
     }
 }
