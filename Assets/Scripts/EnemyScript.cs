9a10494c637f9858588b224def61bbeb8aa3b205
duducorvao <eduardobruno@hotmail.co.uk>
2015-11-25 11:15:36 -0200

    Criado um layer pro collider de baixo pra deixar ele passar os inimigos. Começado o script do inimigo, vou fazer uma "IA" pra ele que será um lerp apenas no X em relação ao X do player

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    public float hp;
+    public float speed;
+    public float speedFactor; //Altera a velocidade do inimigo para gerar uma velocidade diferente para cada um instanciado.
+    public Transform shooterPosition;
+
+    public Transform enemyBullet;
+
+    private GameObject playerReference;
+
+	// Use this for initialization
+	void Start () {
+
+        speed *= Random.Range(1.0f, speedFactor);
+        playerReference = GameObject.FindGameObjectWithTag("Player"); //Acha o objeto player através da tag Player.
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(-transform.up * speed, Space.World);
+        EnemyBehaviour();
+
+
+	}
+
+    private void EnemyBehaviour()
+    {
+        //Lerp apenas de X
+    }
+}
