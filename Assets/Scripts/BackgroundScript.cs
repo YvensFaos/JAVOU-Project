79809774f2b7bce6c46f36fc7b936cbb7a71ac33
duducorvao <eduardobruno@hotmail.co.uk>
2015-11-25 12:56:18 -0200

    Adicionado o background com o script de offset dele

---
+++ Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScript : MonoBehaviour {
+
+	public float speed;
+
+	void Update () 
+	{
+
+        Vector3 offset = new Vector3(0, Time.time * speed, 0);
+        //GetComponent<Renderer>().material.mainTextureOffset = offset;
+
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", offset);
+
+
+	}
+}
